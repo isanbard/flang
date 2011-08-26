9ed247d21cb2a5ecdf9beb53058bbb2311872dcc
Bill Wendling <wendling@apple.com>
2011-08-26 13:47:35 -0700

    A lot of changes.
    * Move Type.{cpp,h} from Basic to AST directories.
    * Add the DeclarationName files. Simplified from clang.
    * Other miscellaneous changes.

---
+++ include/flang/AST/DeclarationName.h
@@ -0,0 +1,309 @@
+//===-- DeclarationName.h - Representation of declaration names -*- C++ -*-===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+//
+// This file declares the DeclarationName and DeclarationNameTable classes.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef FORTRAN_AST_DECLARATIONNAME_H__
+#define FORTRAN_AST_DECLARATIONNAME_H__
+
+#include "flang/Basic/IdentifierTable.h"
+#include "flang/Basic/SourceLocation.h"
+#include "flang/AST/Type.h"
+
+namespace llvm {
+  template <typename T> struct DenseMapInfo;
+}
+
+namespace fortran {
+
+class DeclarationNameTable;
+class IdentifierInfo;
+class MultiKeywordSelector;
+class NamedDecl;
+class TypeSourceInfo;
+
+/// DeclarationName - The name of a declaration. In the common case, this just
+/// stores an IdentifierInfo pointer to a normal name
+class DeclarationName {
+public:
+  /// NameKind - The kind of name this object contains.
+  enum NameKind {
+    Identifier
+  };
+
+private:
+  /// StoredNameKind - The kind of name that is actually stored in the upper
+  /// bits of the Ptr field. This is only used internally.
+  enum StoredNameKind {
+    StoredIdentifier = 0,
+    PtrMask = 0x03
+  };
+
+  /// Ptr - The lowest two bits are used to express what kind of name we're
+  /// actually storing, using the values of NameKind. Depending on the kind of
+  /// name this is, the upper bits of Ptr may have one of several different
+  /// meanings:
+  ///
+  ///   StoredIdentifier - The name is a normal identifier, and Ptr is a normal
+  ///   IdentifierInfo pointer.
+  ///
+  uintptr_t Ptr;
+
+  /// getStoredNameKind - Return the kind of object that is stored in Ptr.
+  StoredNameKind getStoredNameKind() const {
+    return static_cast<StoredNameKind>(Ptr & PtrMask);
+  }
+
+  /// Construct a declaration name from a raw pointer.
+  DeclarationName(uintptr_t Ptr) : Ptr(Ptr) {}
+
+  friend class DeclarationNameTable;
+  friend class NamedDecl;
+
+  /// getFETokenInfoAsVoid - Retrieves the front end-specified pointer
+  /// for this name as a void pointer.
+  void *getFETokenInfoAsVoid() const;
+
+public:
+  /// DeclarationName - Used to create an empty selector.
+  DeclarationName() : Ptr(0) {}
+
+  // Construct a declaration name from an IdentifierInfo*.
+  DeclarationName(const IdentifierInfo *II)
+    : Ptr(reinterpret_cast<uintptr_t>(II)) {
+    assert((Ptr & PtrMask) == 0 && "Improperly aligned IdentifierInfo");
+  }
+
+  // operator bool() - Evaluates true when this declaration name is non-empty.
+  operator bool() const {
+    return ((Ptr & PtrMask) != 0) ||
+           (reinterpret_cast<IdentifierInfo *>(Ptr & ~PtrMask));
+  }
+
+  /// Predicate functions for querying what type of name this is.
+  bool isIdentifier() const { return getStoredNameKind() == StoredIdentifier; }
+
+  /// getNameKind - Determine what kind of name this is.
+  NameKind getNameKind() const;
+
+  /// getAsString - Retrieve the human-readable string for this name.
+  std::string getAsString() const;
+
+  /// printName - Print the human-readable name to a stream.
+  void printName(llvm::raw_ostream &OS) const;
+
+  /// getAsIdentifierInfo - Retrieve the IdentifierInfo* stored in this
+  /// declaration name, or NULL if this declaration name isn't a simple
+  /// identifier.
+  IdentifierInfo *getAsIdentifierInfo() const {
+    if (isIdentifier())
+      return reinterpret_cast<IdentifierInfo *>(Ptr);
+    return 0;
+  }
+
+  /// getAsOpaqueInteger - Get the representation of this declaration name as an
+  /// opaque integer.
+  uintptr_t getAsOpaqueInteger() const { return Ptr; }
+
+  /// getAsOpaquePtr - Get the representation of this declaration name as an
+  /// opaque pointer.
+  void *getAsOpaquePtr() const { return reinterpret_cast<void*>(Ptr); }
+
+  static DeclarationName getFromOpaquePtr(void *P) {
+    DeclarationName N;
+    N.Ptr = reinterpret_cast<uintptr_t> (P);
+    return N;
+  }
+  static DeclarationName getFromOpaqueInteger(uintptr_t P) {
+    DeclarationName N;
+    N.Ptr = P;
+    return N;
+  }
+
+  /// getFETokenInfo/setFETokenInfo - The language front-end is allowed to
+  /// associate arbitrary metadata with some kinds of declaration names,
+  /// including normal identifiers and conversion functions.
+  template<typename T>
+  T *getFETokenInfo() const { return static_cast<T*>(getFETokenInfoAsVoid()); }
+
+  void setFETokenInfo(void *T);
+
+  /// operator== - Determine whether the specified names are identical.
+  friend bool operator==(DeclarationName LHS, DeclarationName RHS) {
+    return LHS.Ptr == RHS.Ptr;
+  }
+
+  /// operator!= - Determine whether the specified names are different.
+  friend bool operator!=(DeclarationName LHS, DeclarationName RHS) {
+    return LHS.Ptr != RHS.Ptr;
+  }
+
+  static DeclarationName getEmptyMarker() {
+    return DeclarationName(uintptr_t(-1));
+  }
+
+  static DeclarationName getTombstoneMarker() {
+    return DeclarationName(uintptr_t(-2));
+  }
+
+  static int compare(DeclarationName LHS, DeclarationName RHS);
+  
+  void dump() const;
+};
+
+/// Ordering on two declaration names. If both names are identifiers, this
+/// provides a lexicographical ordering.
+inline bool operator<(DeclarationName LHS, DeclarationName RHS) {
+  return DeclarationName::compare(LHS, RHS) < 0;
+}
+
+/// Ordering on two declaration names. If both names are identifiers, this
+/// provides a lexicographical ordering.
+inline bool operator>(DeclarationName LHS, DeclarationName RHS) {
+  return DeclarationName::compare(LHS, RHS) > 0;
+}
+
+/// Ordering on two declaration names. If both names are identifiers, this
+/// provides a lexicographical ordering.
+inline bool operator<=(DeclarationName LHS, DeclarationName RHS) {
+  return DeclarationName::compare(LHS, RHS) <= 0;
+}
+
+/// Ordering on two declaration names. If both names are identifiers, this
+/// provides a lexicographical ordering.
+inline bool operator>=(DeclarationName LHS, DeclarationName RHS) {
+  return DeclarationName::compare(LHS, RHS) >= 0;
+}
+
+/// DeclarationNameTable - Used to store and retrieve DeclarationName instances
+/// for the various kinds of declaration names, e.g., normal identifiers, etc.
+class DeclarationNameTable {
+  const ASTContext &Ctx;
+
+  DeclarationNameTable(const DeclarationNameTable&);            // NONCOPYABLE
+  DeclarationNameTable& operator=(const DeclarationNameTable&); // NONCOPYABLE
+
+public:
+  DeclarationNameTable(const ASTContext &C) : Ctx(C) {}
+  ~DeclarationNameTable();
+
+  /// getIdentifier - Create a declaration name that is a simple
+  /// identifier.
+  DeclarationName getIdentifier(const IdentifierInfo *ID) {
+    return DeclarationName(ID);
+  }
+};
+
+/// DeclarationNameLoc - Additional source/type location info for a declaration
+/// name. Needs a DeclarationName in order to be interpreted correctly.
+struct DeclarationNameLoc {
+  union {
+    // The source location for identifier stored elsewhere.
+    // struct {} Identifier;
+
+    // Type info for constructors, destructors and conversion functions.
+    // Locations (if any) for the tilde (destructor) or operator keyword
+    // (conversion) are stored elsewhere.
+    struct {
+      TypeSourceInfo *TInfo;
+    } NamedType;
+  };
+
+  DeclarationNameLoc(DeclarationName Name);
+  // FIXME: this should go away once all DNLocs are properly initialized.
+  DeclarationNameLoc() { std::memset((void*) this, 0, sizeof(*this)); }
+};
+
+/// DeclarationNameInfo - A collector data type for bundling together a
+/// DeclarationName and the correspnding source/type location info.
+struct DeclarationNameInfo {
+private:
+  /// Name - The declaration name, also encoding name kind.
+  DeclarationName Name;
+  /// Loc - The main source location for the declaration name.
+  llvm::SMLoc NameLoc;
+  /// Info - Further source/type location info for special kinds of names.
+  DeclarationNameLoc LocInfo;
+
+public:
+  DeclarationNameInfo(DeclarationName Name, llvm::SMLoc NameLoc)
+    : Name(Name), NameLoc(NameLoc), LocInfo(Name) {}
+
+  DeclarationNameInfo(DeclarationName Name, llvm::SMLoc NameLoc,
+                      DeclarationNameLoc LocInfo)
+    : Name(Name), NameLoc(NameLoc), LocInfo(LocInfo) {}
+
+  /// getName - Returns the embedded declaration name.
+  DeclarationName getName() const { return Name; }
+  /// setName - Sets the embedded declaration name.
+  void setName(DeclarationName N) { Name = N; }
+
+  /// getLoc - Returns the main location of the declaration name.
+  llvm::SMLoc getLoc() const { return NameLoc; }
+  /// setLoc - Sets the main location of the declaration name.
+  void setLoc(llvm::SMLoc L) { NameLoc = L; }
+
+  const DeclarationNameLoc &getInfo() const { return LocInfo; }
+  DeclarationNameLoc &getInfo() { return LocInfo; }
+  void setInfo(const DeclarationNameLoc &Info) { LocInfo = Info; }
+
+  /// getAsString - Retrieve the human-readable string for this name.
+  std::string getAsString() const;
+
+  /// printName - Print the human-readable name to a stream.
+  void printName(llvm::raw_ostream &OS) const;
+
+  /// getBeginLoc - Retrieve the location of the first token.
+  llvm::SMLoc getBeginLoc() const { return NameLoc; }
+  /// getEndLoc - Retrieve the location of the last token.
+  llvm::SMLoc getEndLoc() const;
+  /// getSourceRange - The range of the declaration name.
+  SourceRange getSourceRange() const {
+    return SourceRange(getBeginLoc(), getEndLoc());
+  }
+};
+
+inline llvm::raw_ostream &operator<<(llvm::raw_ostream &OS,
+                                     DeclarationNameInfo DNInfo) {
+  DNInfo.printName(OS);
+  return OS;
+}
+
+}  // end namespace fortran
+
+namespace llvm {
+
+/// Define DenseMapInfo so that DeclarationNames can be used as keys in DenseMap
+/// and DenseSets.
+template<>
+struct DenseMapInfo<fortran::DeclarationName> {
+  static inline fortran::DeclarationName getEmptyKey() {
+    return fortran::DeclarationName::getEmptyMarker();
+  }
+
+  static inline fortran::DeclarationName getTombstoneKey() {
+    return fortran::DeclarationName::getTombstoneMarker();
+  }
+
+  static unsigned getHashValue(fortran::DeclarationName);
+
+  static inline bool
+  isEqual(fortran::DeclarationName LHS, fortran::DeclarationName RHS) {
+    return LHS == RHS;
+  }
+};
+
+template <>
+struct isPodLike<fortran::DeclarationName> { static const bool value = true; };
+
+}  // end namespace llvm
+
+#endif
---
+++ lib/AST/DeclarationName.cpp
@@ -0,0 +1,157 @@
+//===-- DeclarationName.cpp - Declaration names implementation ------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+//
+// This file implements the DeclarationName and DeclarationNameTable classes.
+//
+//===----------------------------------------------------------------------===//
+
+#include "flang/AST/ASTContext.h"
+#include "flang/AST/Decl.h"
+#include "flang/AST/DeclarationName.h"
+#include "flang/Basic/IdentifierTable.h"
+#include "llvm/ADT/DenseMap.h"
+#include "llvm/ADT/FoldingSet.h"
+#include "llvm/Support/ErrorHandling.h"
+#include "llvm/Support/raw_ostream.h"
+using namespace fortran;
+
+//===----------------------------------------------------------------------===//
+// DeclarationName Implementation
+//===----------------------------------------------------------------------===//
+
+DeclarationName::NameKind DeclarationName::getNameKind() const {
+  switch (getStoredNameKind()) {
+  default: break;
+  case StoredIdentifier:
+    return Identifier;
+  }
+
+  // Can't actually get here.
+  assert(false && "This should be unreachable!");
+  return Identifier;
+}
+
+std::string DeclarationName::getAsString() const {
+  std::string Result;
+  llvm::raw_string_ostream OS(Result);
+  printName(OS);
+  return OS.str();
+}
+
+void DeclarationName::printName(llvm::raw_ostream &OS) const {
+  switch (getNameKind()) {
+  case Identifier:
+    if (const IdentifierInfo *II = getAsIdentifierInfo())
+      OS << II->getName();
+    return;
+  }
+
+  assert(false && "Unexpected declaration name kind");
+}
+
+void *DeclarationName::getFETokenInfoAsVoid() const {
+  switch (getNameKind()) {
+  case Identifier:
+    return getAsIdentifierInfo()->getFETokenInfo<void>();
+  }
+
+  assert(false && "Declaration name has no FETokenInfo");
+  return 0;
+}
+
+void DeclarationName::setFETokenInfo(void *T) {
+  switch (getNameKind()) {
+  case Identifier:
+    getAsIdentifierInfo()->setFETokenInfo(T);
+    break;
+  }
+
+  assert(false && "Declaration name has no FETokenInfo");
+}
+
+int DeclarationName::compare(DeclarationName LHS, DeclarationName RHS) {
+  if (LHS.getNameKind() != RHS.getNameKind())
+    return (LHS.getNameKind() < RHS.getNameKind() ? -1 : 1);
+
+  switch (LHS.getNameKind()) {
+  case DeclarationName::Identifier: {
+    IdentifierInfo *LII = LHS.getAsIdentifierInfo();
+    IdentifierInfo *RII = RHS.getAsIdentifierInfo();
+    if (!LII) return RII ? -1 : 0;
+    if (!RII) return 1;
+
+    return LII->getName().compare(RII->getName());
+  }
+  }
+
+  return 0;
+}
+
+void DeclarationName::dump() const {
+  printName(llvm::errs());
+  llvm::errs() << '\n';
+}
+
+//===----------------------------------------------------------------------===//
+// DeclarationNameTable Implementation
+//===----------------------------------------------------------------------===//
+
+DeclarationNameTable::~DeclarationNameTable() {}
+
+//===----------------------------------------------------------------------===//
+// DeclarationNameLoc Implementation
+//===----------------------------------------------------------------------===//
+
+DeclarationNameLoc::DeclarationNameLoc(DeclarationName Name) {
+  switch (Name.getNameKind()) {
+  case DeclarationName::Identifier:
+    break;
+  }
+}
+
+//===----------------------------------------------------------------------===//
+// DeclarationNameInfo Implementation
+//===----------------------------------------------------------------------===//
+
+std::string DeclarationNameInfo::getAsString() const {
+  std::string Result;
+  llvm::raw_string_ostream OS(Result);
+  printName(OS);
+  return OS.str();
+}
+
+void DeclarationNameInfo::printName(llvm::raw_ostream &OS) const {
+  switch (Name.getNameKind()) {
+  case DeclarationName::Identifier:
+    Name.printName(OS);
+    return;
+  }
+
+  assert(false && "Unexpected declaration name kind");
+}
+
+llvm::SMLoc DeclarationNameInfo::getEndLoc() const {
+  switch (Name.getNameKind()) {
+  case DeclarationName::Identifier:
+    return NameLoc;
+  }
+
+  assert(false && "Unexpected declaration name kind");
+  return llvm::SMLoc();
+}
+
+//===----------------------------------------------------------------------===//
+// DenseMapInfo Implementation
+//===----------------------------------------------------------------------===//
+
+unsigned
+llvm::DenseMapInfo<fortran::DeclarationName>::
+getHashValue(fortran::DeclarationName N) {
+  return DenseMapInfo<void*>::getHashValue(N.getAsOpaquePtr());
+}
