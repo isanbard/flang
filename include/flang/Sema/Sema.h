1a91b391167e4060d137af49a7ccc6853869ec21
Bill Wendling <wendling@apple.com>
2011-09-04 18:01:33 -0700

    Add beginnings of Sema files. This just pushes and pops the DeclContext.

---
+++ include/flang/Sema/Sema.h
@@ -0,0 +1,51 @@
+//===--- Sema.h - Semantic Analysis & AST Building --------------*- C++ -*-===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+//
+// This file defines the Sema class, which performs semantic analysis and builds
+// ASTs.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef FLANG_SEMA_SEMA_H__
+#define FLANG_SEMA_SEMA_H__
+
+#include "llvm/Support/SourceMgr.h"
+
+namespace flang {
+
+class ASTContext;
+class DeclContext;
+class Expr;
+
+/// Sema - This implements semantic analysis and AST buiding for Fortran.
+class Sema {
+  Sema(const Sema&);           // DO NOT IMPLEMENT
+  void operator=(const Sema&); // DO NOT IMPLEMENT
+public:
+  typedef Expr ExprTy;
+
+  ASTContext &Context;
+  llvm::SourceMgr &SrcMgr;
+
+  /// CurContext - This is the current declaration context of parsing.
+  DeclContext *CurContext;
+
+  Sema(ASTContext &ctxt, llvm::SourceMgr &sm);
+  ~Sema();
+
+  DeclContext *getContainingDC(DeclContext *DC);
+
+  /// Set the current declaration context until it gets popped.
+  void PushDeclContext(DeclContext *DC);
+  void PopDeclContext();
+};
+
+} // end flang namespace
+
+#endif
---
+++ lib/Sema/Sema.cpp
@@ -0,0 +1,42 @@
+//===--- Sema.cpp - AST Builder and Semantic Analysis Implementation ------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+//
+// This file implements the actions class which performs semantic analysis and
+// builds an AST out of a parse stream.
+//
+//===----------------------------------------------------------------------===//
+
+#include "flang/Sema/Sema.h"
+#include "flang/AST/Decl.h"
+using namespace flang;
+
+Sema::Sema(ASTContext &ctxt, llvm::SourceMgr &sm)
+  : Context(ctxt), SrcMgr(sm), CurContext(0) {}
+
+Sema::~Sema() {}
+
+// getContainingDC - Determines the context to return to after temporarily
+// entering a context.  This depends in an unnecessarily complicated way on the
+// exact ordering of callbacks from the parser.
+DeclContext *Sema::getContainingDC(DeclContext *DC) {
+  return DC->getParent();
+}
+
+void Sema::PushDeclContext(DeclContext *DC) {
+  assert(getContainingDC(DC) == CurContext &&
+      "The next DeclContext should be lexically contained in the current one.");
+  CurContext = DC;
+}
+
+void Sema::PopDeclContext() {
+  assert(CurContext && "DeclContext imbalance!");
+
+  CurContext = getContainingDC(CurContext);
+  assert(CurContext && "Popped translation unit!");
+}
